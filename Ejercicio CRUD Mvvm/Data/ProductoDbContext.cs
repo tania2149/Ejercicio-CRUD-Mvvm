83a61bd85e858fc5e438ba9b3c2ca4544a085641
Tania <yolisma2149@gmail.com>
2024-06-17 20:42:02 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio CRUD Mvvm/Data/ProductoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_CRUD_Mvvm.Data
+{
+    using Ejercicio_CRUD_Mvvm.Models;
+
+    public class ProductoDbContext : DbContext
+    {
+        public ProductoDbContext(DbContextOptions<ProductoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ Ejercicio CRUD Mvvm/Services/ProductoService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EjercicioCRUDMvvm.Data;
+using EjercicioCRUDMvvm.Models;
+
+public class ProductoService
+{
+    private readonly ProductoDbContext _context;
+
+    public ProductoService(ProductoDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Producto>> GetProductosAsync()
+    {
+        return await _context.Productos.ToListAsync();
+    }
+
+    public async Task<Producto> GetProductoAsync(int id)
+    {
+        return await _context.Productos.FindAsync(id);
+    }
+
+    public async Task CreateProductoAsync(Producto producto)
+    {
+        _context.Productos.Add(producto);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateProductoAsync(Producto producto)
+    {
+        _context.Productos.Update(producto);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteProductoAsync(int id)
+    {
+        var producto = await GetProductoAsync(id);
+        if (producto != null)
+        {
+            _context.Productos.Remove(producto);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
